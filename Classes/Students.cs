38d8fc36324b0ed18f55835fcb9fccda18fd6c29
nedkom21 <nedkom21@gmail.com>
2023-05-16 23:40:58 +0300

    XML comments updated

--- Classes/Students.cs
+++ Classes/Students.cs
@@ -7,19 +7,9 @@ using System.Threading.Tasks;
 
 namespace Students_Grades
 {
-    /// <summary>
-    /// Represents a student with a name and grade.
-    /// </summary>
     public class Students : IStudents
     {
-        /// <summary>
-        /// Implements the Name property from the IStudents interface.
-        /// </summary>
         public string Name { get; set; }
-
-        /// <summary>
-        /// Implements the Grade property from the IStudents interface.
-        /// </summary>
         public int Grade { get; set; }
     }
 }
--- Interfaces/IFileHandling.cs
+++ Interfaces/IFileHandling.cs
@@ -6,21 +6,18 @@ using System.Threading.Tasks;
 
 namespace Students_Grades.Interfaces
 {
-    /// <summary>
-    /// Represents an interface for file handling related operations.
-    /// </summary>
     internal interface IFileHandling
     {
         /// <summary>
-        /// Reads student data from a file and returns a list of students.
+        /// Reads a list of students from a file.
         /// </summary>
-        /// <returns>A list of students read from the file.</returns>
+        /// <returns>A list of students.</returns>
         List<Students> Read();
 
         /// <summary>
-        /// Writes student data to a file and returns a list of students.
+        /// Writes a list of students to a file.
         /// </summary>
-        /// <returns>A list of students that were written to the file.</returns>
+        /// <returns>A list of students.</returns>
         List<Students> Write();
     }
 }
